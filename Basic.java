a236f6a616e7edb5e3bcb487363a6659868690dc
Prachi Verma <56695669+Prachi-Verma2522@users.noreply.github.com>
2022-01-22 22:45:52 +0530

    Create Basic.java

---
+++ Basic.java
@@ -0,0 +1,15 @@
+public class Basic {
+
+	public static void main(String[] args) {
+		//A java program is a program with which a class having a main method.
+		// main is a method which will be executed automatically whenever we run our program
+		// whatever we write in the main method will be executed in a sequence
+    
+		System.out.println("Welcome to our java course!");
+		System.out.println("It's not that much tough as we think");
+		System.out.println("Thank you:)");
+    
+		//Byte Codes: they will be generated whenever we compile our program.
+	}
+
+}
