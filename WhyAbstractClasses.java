367ffc208c1ece25b9ac511dbe6126218a3281e7
Prachi Verma <56695669+Prachi-Verma2522@users.noreply.github.com>
2022-02-01 02:37:49 +0530

    Create WhyAbstractClasses.java

---
+++ WhyAbstractClasses.java
@@ -0,0 +1,54 @@
+final class RazorPay{
+	
+	boolean isBankInterfaceUp = true;// can be set to false also
+	RazorPayCallBacks callbacks; // reference variable of the RazorPayCallBacks
+
+	void pay(int amount) {
+		if(isBankInterfaceUp) {
+			System.out.println("[RazorPay] Payment Done for amount: "+amount);
+			callbacks.onPaymentSuccess("Payment Received Successfully");
+		}else {
+			System.out.println("[RazorPay] Payment Failed for amount: "+amount);
+			callbacks.onPaymentFailed("Payment Failed");
+		}
+	}
+	// Polymorphic Statement
+	void on(RazorPayCallBacks callbacks) { // RazorPayCallBacks callbacks = this;
+		this.callbacks = callbacks;
+	}
+}
+abstract class RazorPayCallBacks{
+	abstract void onPaymentSuccess(String message);
+	abstract void onPaymentFailed(String message);
+}
+class ZomatoPaymentsPage extends RazorPayCallBacks{
+	
+	void payForOrder(int amount) {
+		RazorPay payRef = new RazorPay(); // execute RazorPay Code
+		// this -> reference to the current object of ZomatoPaymentsPage
+		payRef.on(this); // let us pass the reference to the current object of ZomatoPaymentsPage
+		payRef.pay(amount);
+	}
+	void onPaymentSuccess(String message) {
+		System.out.println("[ZOMATO] Payment Received");
+		System.out.println("[ZOMATO] Your Order will be delivered soon");
+	}
+	
+	void onPaymentFailed(String message) {
+		System.err.println("[ZOMATO] Payment Failed");//err will show the code in red color
+		System.err.println("[ZOMATO] Please try with some other payment method");
+	}	
+}
+
+public class WhyAbstractClasses {
+
+	public static void main(String[] args) {
+	
+		ZomatoPaymentsPage page = new ZomatoPaymentsPage();
+		//System.out.println(page); ->hashcode
+		page.payForOrder(2000);
+
+	}
+
+}
+
